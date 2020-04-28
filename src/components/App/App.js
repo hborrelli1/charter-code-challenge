f21b3cb858c7803ae2c6d490d4eefbd64d599f82
Harry Borrelli <hborrelli@outlook.com>
2020-04-28 12:02:19 -0600

    Create app folder structure.

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import RestaurantContainer from '../RestaurantContainer/RestaurantContainer';
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <h1>Restaurant Data</h1>
+      </header>
+      <RestaurantContainer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/RestaurantContainer/RestaurantContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const RestaurantContainer = (props) => {
+  return (
+    <div>Restaurant Container</div>
+  )
+}
+
+export default RestaurantContainer;
